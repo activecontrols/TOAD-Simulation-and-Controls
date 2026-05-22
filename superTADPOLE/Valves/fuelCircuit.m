184c9fe1050bcb67bb68c177ca2141a30265bd82
Icequeen3333 <alex2cool4@gmail.com>
2026-05-21 23:10:59 -0400

    fixing some parameters and in general trying to fix engine sim

--- superTADPOLE/Valves/fuelCircuit.m
+++ superTADPOLE/Valves/fuelCircuit.m
@@ -23,11 +23,13 @@ function valve_coeff_fu_cmd = fuelCircuit(chamber_pressure_m, injector_pressure_
     discharge_coeff_fu = 0.65; % [unitless],  -------------Random value just to get the code to run
     orifice_area_ox = 5E-05; % [m^2],  -------------Random value just to get the code to run
     orifice_area_fu = 5E-05; % [m^2],  -------------Random value just to get the code to run
-    friction_pressure_drop_fu = 2E05 ; % [Pa],  -------------Random value just to get the code to run
+    
+    % 150 psi at max thrust, quarter at 50%
+    friction_pressure_drop_fu = 1E06 ; % [Pa],  -------------Random value just to get the code to run
     rho_water = constantsSTADPOLE.dens_w; % [kg/m^3]
     
     % Feedback trim variables
-    k_fu = 1E-10; % [unitless], integral gain for fuel trim,  -------------Random value just to get the code to run
+    k_fu = 0;%1E-10; % [unitless], integral gain for fuel trim,  -------------Random value just to get the code to run
     time_step = 0.01; % [s], based on loop time,  -------------Random value just to get the code to run
 
     persistent integral_error_fu
@@ -37,18 +39,18 @@ function valve_coeff_fu_cmd = fuelCircuit(chamber_pressure_m, injector_pressure_
     end
 
     % Valve coefficient equations
-    delta_p_injector_ox_m = injector_pressure_ox_m - chamber_pressure_m; % [Pa]
+    chamber_pressure_m
+    delta_p_injector_ox_m = injector_pressure_ox_m - chamber_pressure_m % [Pa]
     term_1 = sqrt(2 * delta_p_injector_ox_m * rho_ox); % [kg/m^2-s]
     massflow_ox_m = discharge_coeff_ox * orifice_area_ox * term_1; % [kg/s]
-    massflow_fu_cmd = massflow_ox_m / of_ratio; % [kg/s]
-    term_2 = (massflow_fu_cmd ^ 2) / (2 * rho_fu * (discharge_coeff_fu * orifice_area_fu) ^ 2); % [Pa]
-    injector_pressure_fu_cmd = chamber_pressure_m + term_2; % [Pa]
-    term_3 = sqrt(1 / (rho_fu * rho_water * (tank_pressure_fu_m - friction_pressure_drop_fu - injector_pressure_fu_cmd))); % [m^3.5s/kg^1.5]
-    valve_coeff_fu_cmd = massflow_fu_cmd * term_3; % [m^3.5/kg^0.5]
+    massflow_fu_cmd = massflow_ox_m / of_ratio % [kg/s]
+    injector_pressure_fu_cmd = chamber_pressure_m + (massflow_fu_cmd ^ 2) / (2 * rho_fu * (discharge_coeff_fu * orifice_area_fu) ^ 2) % [Pa]
+    % Can cause imaginary values
+    valve_coeff_fu_cmd = massflow_fu_cmd *  sqrt(1 / (rho_fu * rho_water * (tank_pressure_fu_m - friction_pressure_drop_fu - injector_pressure_fu_cmd))); % [m^3.5/kg^0.5]
     
     % Feedback trim equations
-    error_fu = injector_pressure_fu_cmd - injector_pressure_fu_m; % [Pa]
+    error_fu = injector_pressure_fu_cmd - injector_pressure_fu_m % [Pa]
     integral_error_fu = integral_error_fu + error_fu * time_step;
     trim_fu = k_fu * integral_error_fu;
-    valve_coeff_fu_cmd = valve_coeff_fu_cmd + trim_fu; % [m^3.5/kg^0.5]
+    valve_coeff_fu_cmd = 0.3;%valve_coeff_fu_cmd + trim_fu % [m^3.5/kg^0.5]
 
--- superTADPOLE/Valves/oxygenCircuit.m
+++ superTADPOLE/Valves/oxygenCircuit.m
@@ -23,11 +23,11 @@ function valve_coeff_ox_cmd = oxygenCircuit(thrust_cmd, chamber_pressure_m, tank
     rho_ox = constantsSTADPOLE.dens_o; % [kg/m^3]
     discharge_coeff_ox = 0.65; % [unitless],  -------------Random value just to get the code to run
     orifice_area_ox = 5E-05; % [m^2],  -------------Random value just to get the code to run
-    friction_pressure_drop_ox = 2E05; % [Pa],  -------------Random value just to get the code to run
+    friction_pressure_drop_ox = 68947.6;% was told that pablo neglected it2E05; % [Pa],  -------------Random value just to get the code to run
     rho_water = constantsSTADPOLE.dens_w; % [kg/m^3]
     
     % Feedback trim variables
-    k_ox = 1E-10; % [unitless], integral gain for oxygen trim,  -------------Random value just to get the code to run
+    k_ox = 0;%5E-11; % [unitless], integral gain for oxygen trim,  -------------Random value just to get the code to run
     time_step = 0.01; % [s], based on loop time,  -------------Random value just to get the code to run
 
     persistent integral_error_ox
@@ -37,17 +37,16 @@ function valve_coeff_ox_cmd = oxygenCircuit(thrust_cmd, chamber_pressure_m, tank
     end
 
     % Valve coefficient equations
+    chamber_pressure_m
     chamber_pressure_cmd = thrust_cmd / (throat_area * thrust_coeff); % [Pa]
     massflow_tot_cmd = chamber_pressure_cmd * throat_area / char_vel; % [kg/s]
     massflow_ox_cmd = massflow_tot_cmd * of_ratio / (of_ratio + 1); % [kg/s]
-    term_1 = (massflow_ox_cmd ^ 2) / (2 * rho_ox * (discharge_coeff_ox * orifice_area_ox) ^ 2); % [Pa]
-    injector_pressure_ox_cmd = chamber_pressure_cmd + term_1; % [Pa]
-    term_2 = sqrt(1 / (rho_ox * rho_water * (tank_pressure_ox_m - friction_pressure_drop_ox - injector_pressure_ox_cmd))); % [m^3.5s/kg^1.5]
-    valve_coeff_ox_cmd = massflow_ox_cmd * term_2; % [m^3.5/kg^0.5]
+    injector_pressure_ox_cmd = chamber_pressure_cmd + (massflow_ox_cmd ^ 2) / (2 * rho_ox * (discharge_coeff_ox * orifice_area_ox) ^ 2); % [Pa]
+    valve_coeff_ox_cmd = massflow_ox_cmd * sqrt(1 / (rho_ox * rho_water * (tank_pressure_ox_m - friction_pressure_drop_ox - injector_pressure_ox_cmd))) % [m^3.5/kg^0.5]
     
     % Feedback trim equations
-    error_ox = chamber_pressure_cmd - chamber_pressure_m; % [Pa]
+    error_ox = chamber_pressure_cmd - chamber_pressure_m % [Pa]
     integral_error_ox = integral_error_ox + error_ox * time_step;
     trim_ox = k_ox * integral_error_ox;
-    valve_coeff_ox_cmd = valve_coeff_ox_cmd + trim_ox; % [m^3.5/kg^0.5]
+    valve_coeff_ox_cmd = 0.3%valve_coeff_ox_cmd + trim_ox % [m^3.5/kg^0.5]
 
