dec47ca14dc4aea3c280381db08b780bf3a2ec0b
Icequeen3333 <alex2cool4@gmail.com>
2026-08-19 15:36:35 -0400

    and today on how did alex break everything: good question!

--- TOAD/Controls/LESO/LESO_Thrust.m
+++ TOAD/Controls/LESO/LESO_Thrust.m
@@ -1,8 +1,6 @@
 %% This file contains a sequence of two parallel Linear Extended State Observers
 % The main goal of LESO's is to estimate disturbances felt by the vehicle
-% online and correct for them quickly. The sequence contains two parallel
-% LESOs: A 3 channel coupled LESO for Attitude estimating the net
-% torque-equivalent disturbance, and a single channel scalar LESO
+% online and correct for them quickly. A 3 channel scalar LESO
 % estimating the equivalent thrust disturbance.
 
 function [D_Att, D_Thrust, U_corr] = LESO_Thrust(GND, X_est, X_trg, U_trg, L_Att, L_Thrust, constantsTOAD, t)
@@ -68,27 +66,25 @@ function [D_Att, D_Thrust, U_corr] = LESO_Thrust(GND, X_est, X_trg, U_trg, L_Att
     %% Second Order thrust LESO
     % tested with solely in the z direction
     idx_thr = [4:6, 7:9];
-    e_grav = [0 0 1; 0 0 1];
-    % body to inertial
-    % C_BI_ref = quatRot(X_trg(1:4));
+    
 
     % A_d_thr = A_d(idx_thr, idx_thr);   
-    b0_thr = B_d(idx_thr, 3)
+    b0_thr = B_d(idx_thr(1:3), 1:4);
     b0_e = b0_thr;
-    % A_LESO_Thr = [1 dT_LESO dT_LESO^2/2;
-    %               0   1         dT_LESO;
-    %               0   0              1];
-    % B_LESO_Thr = [b_0 * dT_LESO^2/2;
-    %                     b_0*dT_LESO;
-    %                              0];
+    A_LESO_Thr = [eye(3,3) dT_LESO*eye(3,3) dT_LESO^2/2*eye(3,3);
+                  zeros(3)   eye(3,3)         dT_LESO*eye(3,3);
+                  zeros(3)   zeros(3)              eye(3,3)];
+    B_LESO_Thr = [b0_thr * dT_LESO.^2/2;
+                        b0_thr*dT_LESO;
+                                 zeros(3,4)];
 
     y_thr_abs = X_est(idx_thr);
     y_thr_abs_e = y_thr_abs([3,6]);
 
-    %xhat_thr_pred = A_LESO_Thr * xhat_thr + B_LESO_Thr * U_thr_abs;
+    xhat_thr_pred = A_LESO_Thr * xhat_thr + B_LESO_Thr * U_trg;
 
     % should end up as a 3x1
-    xhat_thr_pred = X_trg(idx_thr);
+    % xhat_thr_pred = X_trg(idx_thr);
     xhat_thr_pred_e = xhat_thr_pred([3,6]);
 
     % should be a 3x2. need to talk w pablo as to how it isn't
@@ -96,11 +92,15 @@ function [D_Att, D_Thrust, U_corr] = LESO_Thrust(GND, X_est, X_trg, U_trg, L_Att
     
     % forcing a 0 disturbance on the predicted because it's not generated
     % with a matrix and not 3x1
-    xhat_thr = [xhat_thr_pred;0;0;0] + L_Thrust * (y_thr_abs - xhat_thr_pred);
+    xhat_thr = xhat_thr_pred + L_Thrust * (y_thr_abs - xhat_thr_pred(1:6));
 
+
+    e = [0 0 1];
+    % body to inertial
     C_BI_ref = quatRot(X_est(1:4));
     C_IB_ref = C_BI_ref';
-    D_Thrust = C_IB_ref*xhat_thr(7:9);
+    e_thr = e*C_IB_ref;
+    D_Thrust = e_thr*xhat_thr(7:9);
     D_Att = 0;
     
 
@@ -109,20 +109,29 @@ function [D_Att, D_Thrust, U_corr] = LESO_Thrust(GND, X_est, X_trg, U_trg, L_Att
     
     U_corr = zeros(4,1);
     b0_min = 1e-6;
+
+    b0_thr
+    abs(e_thr*b0_thr(:,3))
     
-    if abs(b0_thr(6)) > b0_min
-        U_corr_th = (D_Thrust / b0_thr.^2)
+    if abs(e_thr*b0_thr(:,3)) > b0_min
+        U_corr_th = (D_Thrust ./ e_thr*b0_thr(:,3));
     else
-        U_corr_th = zeros(3,6);
+        U_corr_th = 0;
     end
-    U_corr_th = (C_IB_ref*U_corr_th(1:3,6));
-    U_corr(3) = U_corr_th(3);
+    
+    U_corr(3) = U_corr_th;
     U_corr
 end
 
 
-function [thrust, quat]=quaterror()
+function [thrust, quat] = quaterror(thrust_corr, U_trg)
 %%% 
 % extract the quaternion and thrust for rotation to counter the
 % disturbances
+% Input:
+%   Thrust: x y z components
+
+[cos(theta) * sin(gamma), sin(theta) * cos(gamma), sin(theta) * sin(gamma)]
+
+
 end
