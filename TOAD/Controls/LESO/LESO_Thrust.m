e9405ee0f4b3a9579fd0e3eec9d15fbcc631ea34
Icequeen3333 <alex2cool4@gmail.com>
2026-08-19 20:10:28 -0400

    it's broken in a new funny way now

--- TOAD/Controls/LESO/LESO_Thrust.m
+++ TOAD/Controls/LESO/LESO_Thrust.m
@@ -54,6 +54,7 @@ function [D_Att, D_Thrust, U_corr] = LESO_Thrust(GND, X_est, X_trg, U_trg, L_Att
     T(1:4, 1:3) = 0.5 * XiMat(q_ref);
     T(5:13, 4:12) = eye(9);
 
+    pinv(T)
     A_lin = pinv(T) * Jx * T;
     B_lin = pinv(T) * Ju;
 
@@ -64,41 +65,36 @@ function [D_Att, D_Thrust, U_corr] = LESO_Thrust(GND, X_est, X_trg, U_trg, L_Att
     dT_LESO = 1/1000;
     
     %% Second Order thrust LESO
-    % tested with solely in the z direction
     idx_thr = [4:6, 7:9];
     
+     % body to inertial
+    C_BI_ref = quatRot(X_est(1:4));
+    C_IB_ref = C_BI_ref';
 
     % A_d_thr = A_d(idx_thr, idx_thr);   
-    b0_thr = B_d(idx_thr(1:3), 1:4);
-    b0_e = b0_thr;
+    % b0_thr = B_d(idx_thr(1:3), 1:4);
+
+    b_0 = C_IB_ref/constantsTOAD.m_wet;
+
     A_LESO_Thr = [eye(3,3) dT_LESO*eye(3,3) dT_LESO^2/2*eye(3,3);
                   zeros(3)   eye(3,3)         dT_LESO*eye(3,3);
                   zeros(3)   zeros(3)              eye(3,3)];
-    B_LESO_Thr = [b0_thr * dT_LESO.^2/2;
-                        b0_thr*dT_LESO;
-                                 zeros(3,4)];
+    B_LESO_Thr = [b_0 * dT_LESO.^2/2;
+                         b_0*dT_LESO;
+                         zeros(3,3)];
 
     y_thr_abs = X_est(idx_thr);
-    y_thr_abs_e = y_thr_abs([3,6]);
+    theta = U_trg(1); 
+    phi = U_trg(2);
+    U = U_trg(3) * [cos(theta)*sin(phi); -sin(theta); cos(theta)*cos(phi)];
 
-    xhat_thr_pred = A_LESO_Thr * xhat_thr + B_LESO_Thr * U_trg;
-
-    % should end up as a 3x1
-    % xhat_thr_pred = X_trg(idx_thr);
-    xhat_thr_pred_e = xhat_thr_pred([3,6]);
-
-    % should be a 3x2. need to talk w pablo as to how it isn't
-    L_Thrust_e = L_Thrust(7:9,5:6);
+    xhat_thr_pred = A_LESO_Thr * xhat_thr + B_LESO_Thr * U;
     
-    % forcing a 0 disturbance on the predicted because it's not generated
-    % with a matrix and not 3x1
     xhat_thr = xhat_thr_pred + L_Thrust * (y_thr_abs - xhat_thr_pred(1:6));
 
 
     e = [0 0 1];
-    % body to inertial
-    C_BI_ref = quatRot(X_est(1:4));
-    C_IB_ref = C_BI_ref';
+   
     e_thr = e*C_IB_ref;
     D_Thrust = e_thr*xhat_thr(7:9);
     D_Att = 0;
@@ -110,17 +106,18 @@ function [D_Att, D_Thrust, U_corr] = LESO_Thrust(GND, X_est, X_trg, U_trg, L_Att
     U_corr = zeros(4,1);
     b0_min = 1e-6;
 
-    b0_thr
-    abs(e_thr*b0_thr(:,3))
     
-    if abs(e_thr*b0_thr(:,3)) > b0_min
-        U_corr_th = (D_Thrust ./ e_thr*b0_thr(:,3));
+    abs(e_thr*b_0)
+    
+    if abs(e_thr*b_0) > b0_min
+        U_corr_th = (D_Thrust*constantsTOAD.m_wet);
     else
         U_corr_th = 0;
     end
     
+    U_corr_th
     U_corr(3) = U_corr_th;
-    U_corr
+    U_corr;
 end
 
 
