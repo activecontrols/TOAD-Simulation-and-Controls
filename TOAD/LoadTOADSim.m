64149e0fa0f8ef7eb9d0ca278e47d5eaa8c8e03d
pplata <pplata@X-ARMS2106PC16>
2026-09-02 15:15:29 -0400

    mmm

--- TOAD/LoadTOADSim.m
+++ TOAD/LoadTOADSim.m
@@ -75,7 +75,7 @@ constantsTOAD.OmegaAtt = 0.75;
 
 % Pick a trajectory filename 
 try 
-    filename = "Backflip_v1.csv";
+    filename = "Backflip_v2.csv";
     Data = readmatrix("Guidance\Trajectories\"+filename);
     constantsTOAD.Traj.Time = Data(:, 1);
     constantsTOAD.Traj.States = Data(:, 2:16);
