69e1dfe895f6e6c89f058bf1a65dbb825810b382
Icequeen3333 <alex2cool4@gmail.com>
2026-09-10 19:33:18 -0400

    Specifying .csv while reading and writing gains

--- TOAD/Controls/Time Varying LQI/SaveGains.m
+++ TOAD/Controls/Time Varying LQI/SaveGains.m
@@ -1,13 +1,13 @@
 function SaveGains(filename, constantsTOAD)
-    m = readmatrix("Guidance\Trajectories\"+filename);
+    m = readmatrix("Guidance\Trajectories\"+filename + ".csv");
     t = m(:, 1)';
     x = m(:, 2:16)';
     u = m(1:end-1, 17:20)';
     
     [K_trans_List, K_rot_List, LA, LT] = GainGenerator(x, u, t, constantsTOAD);
     
-    writematrix(K_trans_List, "Guidance\Trajectories\Gains\K_trans_" + filename);
-    writematrix(K_rot_List,   "Guidance\Trajectories\Gains\K_rot_" + filename);
-    writematrix(LA,           "Guidance\Trajectories\Gains\LA_" + filename);
-    writematrix(LT,           "Guidance\Trajectories\Gains\LT_" + filename);
+    writematrix(K_trans_List, "Guidance\Trajectories\Gains\K_trans_" + filename + ".csv");
+    writematrix(K_rot_List,   "Guidance\Trajectories\Gains\K_rot_" + filename + ".csv");
+    writematrix(LA,           "Guidance\Trajectories\Gains\LA_" + filename + ".csv");
+    writematrix(LT,           "Guidance\Trajectories\Gains\LT_" + filename + ".csv");
 end
